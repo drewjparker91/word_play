c5b4f173a66e978576c2c22456345fb88fafb87d
Ben Stoller <stollersystemsound@gmail.com>
2020-08-17 15:06:57 -0700

    add initial js
    Co-authored-by: Alma  Frankenstein <a.q.frankenstein@gmail.com>
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+// Business Logic 
+function testSplit(input) {
+  console.log(input);
+  let splitInput = input.split();
+  console.log(splitInput);
+  return splitInput;  
+ 
+}
+
+
+
+
+// Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
+    const sentence = $("input#fav3").val();
+    $("#returnSentence").text(testSplit(sentence));
     event.preventDefault();
+    
+    $("#returnSentence").show();
   }); 
 });
