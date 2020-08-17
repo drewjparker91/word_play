2bc11c11fb1e083c8b0db05da8f2abdfcd96ce9e
Ben Stoller <stollersystemsound@gmail.com>
2020-08-17 15:32:38 -0700

    get business logic started and working
    Co-authored-by: Alma  Frankenstein <a.q.frankenstein@gmail.com>
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 // Business Logic 
 function testSplit(input) {
   console.log(input);
-  let splitInput = input.split();
+  let splitInput = input.split(" ");
   console.log(splitInput);
-  return splitInput;  
- 
+  return splitInput;
+  // plusThree(splitInput);   
 }
 
-
+function plusThree(input){
+  longerThanThree = [];
+  input.forEach(function(word) {
+    if (word.length >= 3){
+      longerThanThree.push(word);
+    }
+  })
+  console.log(longerThanThree);
+  //longerThanThree.join();
+  return longerThanThree;
+};
 
 
 // Interface Logic
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     const sentence = $("input#fav3").val();
-    $("#returnSentence").text(testSplit(sentence));
+    let splitSentence = testSplit(sentence);
+    let moreThanThree = plusThree(splitSentence);
+    $("#returnSentence").text(moreThanThree);
     event.preventDefault();
     
     $("#returnSentence").show();
